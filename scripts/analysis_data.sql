232bf915462baf99535416792a539713f69efbfd
athirahgit-02 <athirahasri02@gmail.com>
2026-04-16 11:39:23 +0800

    Update data insight

--- scripts/analysis_data.sql
+++ scripts/analysis_data.sql
@@ -22,28 +22,11 @@ GROUP BY activity_level
 ORDER BY avg_minutes_asleep DESC;  
 
 /* Observation: Counter-intuitively, 'Highly Active' users show higher minutes awake in bed (avg_awake_mins). 
-Potential Recommendation: Investigate 'over-training' alerts for high-step users. 
-*/  
-
--- ====================================================================================
--- Insight 2: Sleep Effieciency  
--- Purpose: Identify the percentage of users falling below the 85% healthy sleep threshold  
--- ====================================================================================  
-
-SELECT 
-    Id,
-    ROUND(AVG((TotalMinutesAsleep / TotalTimeInBed) * 100), 2) AS avg_efficiency_pct
-FROM `bellabeat_data.combined_step_sleep`
-GROUP BY Id
-HAVING avg_efficiency_pct < 85
-ORDER BY avg_efficiency_pct ASC;  
-
-/* Observation: 2 users fall below 85% sleep efficiency. 
-Potential Recommendation: Target this segment with breathing or meditation app features.
+Potential Recommendation: Implement a dynamic "Cooldown" notification (eg. recommending guided breathing to lower heart rate)
 */  
 
 -- =========================================================================
--- Insight 3: Weekly Usage Pattern
+-- Insight 2: Weekly Usage Pattern
 -- Purpose: Determine which days users are most active vs. most rested.
 -- =========================================================================
 
@@ -51,11 +34,35 @@ SELECT
     FORMAT_DATE('%A', ActivityDate) AS day_of_week,
     EXTRACT(DAYOFWEEK FROM ActivityDate) AS day_num,
     ROUND(AVG(StepTotal), 0) AS avg_steps,
-    ROUND(AVG(TotalMinutesAsleep), 0) AS avg_sleep
+    ROUND(AVG(TotalMinutesAsleep), 0) AS avg_minutes_asleep
 FROM `bellabeat_data.combined_step_sleep`
-GROUP BY day_of_week, day_num
+GROUP BY day_of_week, day_num 
 ORDER BY day_num;
 
 /* Observation: Saturday peaks in activity, Sunday peaks in sleep. 
-Potential Recommendation: Market "Sunday Reset" notifications to encourage recovery. 
-*/
+Potential Recommendation: Market "Sunday Reset" notifications to encourage recovery (eg. suggesting low impact activities)
+*/  
+
+-- ====================================================================================
+-- Insight 3: Activity Threshold  
+-- Purpose: Identify the 'Optimal Range' where steps maximize sleep efficiency.  
+-- ====================================================================================  
+
+SELECT 
+    CASE 
+        WHEN StepTotal < 5000 THEN '1. Under-Active'
+        WHEN StepTotal BETWEEN 5000 AND 8000 THEN '2. Moderate (Sweet Spot)'
+        WHEN StepTotal BETWEEN 8001 AND 12000 THEN '3. High Activity'
+        ELSE '4. Extreme Activity'
+    END AS activity_level,
+    ROUND(AVG((TotalMinutesAsleep / TotalTimeInBed) * 100), 2) AS avg_efficiency_pct,
+    ROUND(AVG(TotalMinutesAsleep), 0) AS avg_minutes_asleep
+FROM `bellabeat_data.combined_step_sleep`
+GROUP BY activity_level
+ORDER BY activity_level;
+
+/* Observation: Users in the 'Moderate' zone (5k-8k steps) show the highest efficiency. 
+Once users cross the 10k-12k threshold, efficiency drops and wakefulness increases.
+Potential Recommendation: Pivoting from a 'More is Better' messaging strategy to an 'Optimal Balance' strategy (warning users when they might be pushing into the 
+'Over-Stimulation Zone' that compromises recovery.
+*/  
